b0575a1f11f87f5304aebfb293363baf7227b023
Jim <shalinor@internode.on.net>
2015-02-28 14:29:52 +1100

    Copied Hello World into repository

---
+++ Hello World/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class MyClass
+    {
+        //Exercise - Classes
+        public int SimpleAdditionFunction(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+                return 0;
+
+            return num1 / num2;
+        }
+    }
+}
---
+++ Hello World/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise - Hello World
+//            Console.Write("Hello World");
+//            while (true) ; //This just keeps our console window open
+
+
+
+            //Exercise - User Input
+            //Console.Write("Please enter your first name: ");
+            //string name = Console.ReadLine();
+
+            //Console.Clear();
+
+            //Console.Write("Hello " + name);
+            //while (true) ;
+
+
+
+            //Exercise - Classes
+            //MyClass myClass = new MyClass();
+
+            //int answer = myClass.SimpleAdditionFunction(3, 4);
+
+            //Console.Write("3 + 4 = " + answer);
+            //Console.ReadKey();
+
+
+
+            //Exercise - Simple Calculator
+            //string number1, number2;
+            //double num1, num2;
+            //MyClass myClass = new MyClass();
+
+            //do
+            //{
+            //    Console.Write("Please enter first number: ");
+            //    number1 = Console.ReadLine();
+
+            //} while (!double.TryParse(number1, out num1));
+
+            //do
+            //{
+            //    Console.Write("Please enter second number: ");
+            //    number2 = Console.ReadLine();
+
+            //} while (!double.TryParse(number2, out num2));
+
+            //Console.WriteLine(num1 + " + " + num2 + " = " + myClass.Add(num1, num2));
+            //Console.WriteLine(num1 + " - " + num2 + " = " + myClass.Subtract(num1, num2));
+            //Console.WriteLine(num1 + " * " + num2 + " = " + myClass.Multiply(num1, num2));
+            //Console.WriteLine(num1 + " / " + num2 + " = " + myClass.Divide(num1, num2));
+            //Console.WriteLine("Please press any key to exit...");
+            //Console.ReadKey();
+
+            
+
+            //Exercise - Testing Anagrams
+
+
+        }
+    }
+}
